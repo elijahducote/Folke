c6dda57e1227be41153ee36289ef2176d769194c
elijahducote <validpupflower@gmail.com>
2017-11-14 17:29:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 // const prefix = "<@380117580242878484>";
 
 client.on("ready", () => {
-  client.user.setGame(`****down ${client.users.size} players | ${client.guilds.size} server(s)`);
+  client.user.setGame(`**﻿**down ${client.users.size} players | ${client.guilds.size} server(s)`);
 });
 
 
