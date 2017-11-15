98852aaba7ed76428808acaa38bf090f09ef0a04
elijahducote <validpupflower@gmail.com>
2017-11-14 18:11:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 // const prefix = "<@380117580242878484>";
 
-client.on("ready", () => {
-  client.user.setGame(`﻿${client.users.} players | ${client.guilds.size} server(s)`, "https://www.twitch.tv/theoraclegamer");
+client.on('ready', () => {
+  client.user.setGame(`${client.users.size} players | ${client.guilds.size} server(s)`, "https://twitch.tv/theoraclegamer");
 });
 
 
