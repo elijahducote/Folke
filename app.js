4634f44881fc1d71b1f3369a0dfd97f931bb08a7
elijahducote <validpupflower@gmail.com>
2017-11-15 17:20:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ client.on('ready', () => {
   client.user.setGame(` ${client.users.size - 4}  players  |  ${client.guilds.size}  server (s)`);
 });
 
-client.on('message', message => {
-  if (message.content.startsWith(prefix)) {
-    if (message.content.startsWith(prefix)) return;
-    const getter = message.content.slice(prefix.length).trim().split(/ +/g);
+client.on('message', msg => {
+  if (msg.content.startsWith(prefix)) {
+    if (!msg.content.startsWith(prefix)) return;
+    const getter = msg.content.slice(prefix.length).trim().split(/ +/g);
     const command = getter.pop().toLowerCase();
     if (command === 'quack') {
-      message.channel.send('**Q u a c k  .**');
+      msg.channel.send('**Q u a c k  .**');
     }
   }
 });
 
-client.on('message', message => {
-  if (message.content.endsWith(prefix)) {
-    if (message.content.endsWith(prefix)) return;
-    const getter = message.content.slice(prefix.length).trim().split(/ +/g);
+client.on('message', msg => {
+  if (msg.content.endsWith(prefix)) {
+    if (!msg.content.endsWith(prefix)) return;
+    const getter = msg.content.slice(prefix.length).trim().split(/ +/g);
     const command = getter.shift().toLowerCase();
     if (command === 'quack') {
-      message.channel.send('**Q u a c k .**');
+      msg.channel.send('**Q u a c k .**');
     }
   }
 });
