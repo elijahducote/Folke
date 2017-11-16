936ed4f1d77156370d7fff6aa30fd19fe4c3656c
elijahducote <validpupflower@gmail.com>
2017-11-16 15:58:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '<@380117580242878484>';
+const entity = "<@380117580242878484>";
 
-client.on('ready', () => {
+client.on("ready", () => {
   client.user.setGame(` ${client.users.size - 4}  players  |  ${client.guilds.size}  server (s)`);
 });
 
-client.on('message', message => {
-  if (message.content.endsWith(prefix)) {
-    const args = message.content.slice(prefix.length).trim().split(' ');
-    const cmmd = args.shift().toLowerCase();
-    if (cmmd === 'quack') {
-      message.channel.send('Quack');
-    }
-  }
-});
+client.on("message", funcion(message) => {
+  const args = message.content.slice(entity.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-client.on('message', msg => {
-  if (msg.content.startsWith(prefix)) {
-    if (msg.author.bot || !msg.content.startsWith(prefix)) return;
-    const getter = msg.content.slice(prefix.length).trim().split(/ +/g);
-    const command = getter.shift().toLowerCase();
-    if (command === 'quack') {
-      msg.channel.send('Quack');
+  if (message.content.startsWith(entity)) {
+    if (command.indexOf("quack")) {
+      message.channel.send("Quack");
+    }  else {
+         if (message.content.endsWith(entity)) {
+           if (command.indexOf("quack")) {
+              message.channel.send("Quack");
+           }
+         }
     }
   }
 });
